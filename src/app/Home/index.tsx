e8983904a557e32e98575f37fd910f21298c3e71
Luan-Neumann-Dev <luan.neumann.dev@gmail.com>
2026-09-11 10:58:23 -0300

    feat: create the list item

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,12 +1,31 @@
-import { Text, View, Image, TouchableOpacity } from "react-native";
-import { Button } from "@/components/Button";
+import { Text, View, Image, TouchableOpacity, FlatList } from "react-native";
 import { styles } from "./styles";
+import { FilterStatus } from "@/types/FilterStatus";
+
+import { Button } from "@/components/Button";
 import { Input } from "@/components/Input";
 import { Filter } from "@/components/Filter";
-import { FilterStatus } from "@/types/FilterStatus";
+import { Item } from "@/components/Item";
 
 const FILTER_STATUS: FilterStatus[] = [
   FilterStatus.PENDING, FilterStatus.DONE
+];
+const ITEMS = [
+  {
+    id: 1,
+    status: FilterStatus.DONE,
+    description: "1 pacote de café"
+  },
+  {
+    id: 2,
+    status: FilterStatus.PENDING,
+    description: "1 pacotes de macarrão"
+  },
+  {
+    id: 3,
+    status: FilterStatus.PENDING,
+    description: "3 miojos"
+  }
 ]
 
 export function Home() {
@@ -31,6 +50,22 @@ export function Home() {
             <Text style={styles.clearText}>Limpar</Text>
           </TouchableOpacity>
         </View>
+
+        <FlatList 
+          data={ITEMS}
+          keyExtractor={(item) => String(item.id)}
+          renderItem={({item}) => (
+            <Item 
+              data={item}
+              onStatus={() => console.log("oi")}
+              onRemove={() => console.log("oi")}
+            />
+          )}
+          showsVerticalScrollIndicator={false}
+          ItemSeparatorComponent={() => <View style={styles.separator} />}
+          contentContainerStyle={styles.listContent}
+          ListEmptyComponent={() => <Text style={styles.empty}>Nenhum item aqui.</Text>}
+        />
       </View>
     </View>
   )
