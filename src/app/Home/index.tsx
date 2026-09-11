6ea40947302ab71d33d8c0541e1cfbf4ce52f10a
Luan-Neumann-Dev <luan.neumann.dev@gmail.com>
2026-09-11 11:44:59 -0300

    feat: create function to add a new item

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View, Image, TouchableOpacity, FlatList } from "react-native";
+import { Text, View, Image, TouchableOpacity, FlatList, Alert } from "react-native";
 import { styles } from "./styles";
 import { FilterStatus } from "@/types/FilterStatus";
 
@@ -6,43 +6,48 @@ import { Button } from "@/components/Button";
 import { Input } from "@/components/Input";
 import { Filter } from "@/components/Filter";
 import { Item } from "@/components/Item";
+import { useState } from "react";
 
 const FILTER_STATUS: FilterStatus[] = [
   FilterStatus.PENDING, FilterStatus.DONE
 ];
-const ITEMS = [
-  {
-    id: 1,
-    status: FilterStatus.DONE,
-    description: "1 pacote de café"
-  },
-  {
-    id: 2,
-    status: FilterStatus.PENDING,
-    description: "1 pacotes de macarrão"
-  },
-  {
-    id: 3,
-    status: FilterStatus.PENDING,
-    description: "3 miojos"
-  }
-]
 
 export function Home() {
+  const [filter, setFilter] = useState(FilterStatus.PENDING);
+  const [description, setDescription] = useState("")
+  const [items, setItems] = useState<any>([])
+
+  function handleAdd() {
+    if (!description.trim()) {
+      return Alert.alert("Adicionar", "Informe a descrição para adicionar.")
+    }
+
+    const newItem = {
+      id: Math.random().toString(36).substring(2),
+      description,
+      status: FilterStatus.PENDING,
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Image source={require("@/assets/logo.png")} style={styles.logo} />
 
       <View style={styles.form}>
-        <Input placeholder="O que você precisa comprar?" />
-        <Button title="Entrar" />
+        <Input placeholder="O que você precisa comprar?" onChangeText={setDescription}/>
+        <Button title="Adicionar" onPress={handleAdd}/>
       </View>
 
       <View style={styles.content}>
         <View style={styles.header}>
           {
             FILTER_STATUS.map((status) => (
-              <Filter key={status} status={status} isActive />
+              <Filter 
+                key={status} 
+                status={status} 
+                isActive={status === filter}
+                onPress={() => setFilter(status)}
+                />
             ))
           }
 
@@ -52,7 +57,7 @@ export function Home() {
         </View>
 
         <FlatList 
-          data={ITEMS}
+          data={items}
           keyExtractor={(item) => String(item.id)}
           renderItem={({item}) => (
             <Item 
