9456d1c6743b9d90e6ac3c73d32224998d4205b3
Luan-Neumann-Dev <luan.neumann.dev@gmail.com>
2026-09-11 09:37:03 -0300

    feat(component): create status icon component

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -2,6 +2,7 @@ import { Text, TouchableOpacity, TouchableOpacityProps } from "react-native";
 
 import { styles } from "./styles";
 import { FilterStatus } from "@/types/FilterStatus";
+import { StatusIcon } from "../StatusIcon";
 
 type Props = TouchableOpacityProps & {
     status: FilterStatus,
@@ -15,6 +16,7 @@ export function Filter({ status, isActive, ...rest}: Props) {
             activeOpacity={0.8}
             {...rest}
         >
+            <StatusIcon status={status} />
             <Text style={styles.title}>
                 {status === FilterStatus.DONE ? "Comprados" : "Pendentes"}
             </Text>
